05383502c53605a42a3f5c0e2bb7ba5174202d98
Manuela05 <74020547+Manuela05@users.noreply.github.com>
2020-11-26 10:24:09 +0100

    Update esercizio24.py

--- esercizio24.py
+++ esercizio24.py
@@ -3,7 +3,9 @@ candidato2 = int(input("Inserire il numero di voti del secondo candidato:"))
 totale = candidato1 + candidato2
 perccandidato1 = candidato1*100/totale
 perccandidato2 = candidato2*100/totale
-if perccandidato1 > perccandidato2:
+if perccandidato1 == perccandidato2:
+    print("Pareggio!")
+elif perccandidato1 > perccandidato2:
     print("Il vincitore è il primo candidato!")
 else:
     print("Il vincitore è il secondo candidato!")
