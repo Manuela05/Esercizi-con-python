55cfa2ab64ae8422c7f8a45c57eacf549ce65732
Manuela.05 <manuela.manfredelli@gmail.com>
2020-12-10 10:41:55 +0100

    Modifica di calcolo_aree

--- calcolo_aree.py
+++ calcolo_aree.py
@@ -1,80 +1,79 @@
 import math
 
-print("Di quale figura vuoi calcolare l'area?")
-print("1. QUADRATO")
-print("2. RETTANGOLO")
-print("3. TRIANGOLO")
-print("4. CERCHIO")
+print('''Di quale figura vuoi calcolare l'area?
+1. QUADRATO
+2. RETTANGOLO
+3. TRIANGOLO
+4. CERCHIO''')
 
 scelta = input()
 
 if scelta == "1":
-    print("Quale dato conosci? ")
-    print("1. Lato")
-    print("2. Diagonale")
-    print("3. Perimetro")
+    print('''Quale dato conosci?
+    1. Lato
+    2. Diagonale
+    3. Perimetro''')
     dato = input()
     
     if dato == "1":
-        l = int(input("Quanto misura il lato? "))
-        l = float(l)
+        l = float(input("Quanto misura il lato? "))
                 
     elif dato == "2":
-        d = int(input("Quanto misura la diagonale? "))
+        d = float(input("Quanto misura la diagonale? "))
         l = d/(2**(1/2))
         
     else:
-        perimetro = int(input("Quanto misura il perimetro? "))
+        perimetro = float(input("Quanto misura il perimetro? "))
         l = perimetro/4
 
     area = round(l**2,1)
     print("L'area del quadrato misura", area)
     
 if scelta == "2":
-    print("Quale dato conosci? ")
-    print("1. Base e altezza")
-    print("2. Diagonale e base/altezza")
-    print("3. Perimetro e base/altezza")
+    print('''Quale dato conosci?
+    1. Base e altezza
+    2. Diagonale e base/altezza
+    3. Perimetro e base/altezza''')
     dato = input()
     
     if dato == "1":
-        b = int(input("Quanto misura la base? "))
-        h = int(input("Quanto misura l'altezza? "))      
+        b = float(input("Quanto misura la base? "))
+        h = float(input("Quanto misura l'altezza? "))      
         
     elif dato == "2":
-        d = int(input("Quanto misura la diagonale? "))
-        b = int(input("Quanto misura la base/altezza? "))
+        d = float(input("Quanto misura la diagonale? "))
+        b = float(input("Quanto misura la base/altezza? "))
         h = (d**2 - b**2)**(1/2)  
     else:
-        perimetro = int(input("Quanto misura il perimetro? "))
-        b = int(input("Quanto misura la base/altezza? "))
+        perimetro = float(input("Quanto misura il perimetro? "))
+        b = float(input("Quanto misura la base/altezza? "))
         h = (perimetro - 2*b)/2
 
     area = round(b*h,1)
     print("L'area del rettangolo misura", area) 
       
 if scelta == "3":
-    b = int(input("Quanto misura la base? "))
-    h = int(input("Quanto misura l'altezza? "))
+    b = float(input("Quanto misura la base? "))
+    h = float(input("Quanto misura l'altezza? "))
     
     area = round(b*h/2,1)
     print("L'area del triangolo misura", area)
 if scelta == "4":
-    print("Quale dato conosci?")
-    print("1. Raggio")
-    print("2. Diametro")
-    print("3. Circonferenza")
+    print('''Quale dato conosci?
+    1. Raggio
+    2. Diametro
+    3. Circonferenza''')
     dato = input()
     
     if dato == "1":
-        r = int(input("Quanto misura il raggio? "))
+        r = float(input("Quanto misura il raggio? "))
        
     elif dato == "2":
-        d = int(input("Quanto misura il diametro? "))
+        d = float(input("Quanto misura il diametro? "))
         r = d/2  
           
     else:
-        C = int(input("Quanto misura la circonferenza? "))
+        C = float(input("Quanto misura la circonferenza? "))
         r = C/(2*math.pi)
         
     area = round(math.pi*(r**2),1)
